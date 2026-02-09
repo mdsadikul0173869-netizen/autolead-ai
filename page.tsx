74a80ed3feb00122bb720e94183fe824335c9b13
Sadikul Mama <mdsadikul0173869@gmail.com>
2026-02-09 10:19:40 +0600

    added chat window finally

--- page.tsx
+++ page.tsx
@@ -43,7 +43,14 @@ export default function Home() {
             <div key={lead.id} className="border-b py-2">
               <p className="font-bold">{lead.name}</p>
               <p className="text-sm text-gray-500">{lead.email}</p>
-            </div>
+            {/* Floating Chat Window */}
+<div className="fixed bottom-6 right-6 z-50">
+  <div className="bg-red-600 p-4 rounded-full shadow-lg cursor-pointer hover:bg-red-700 transition-all">
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+    </svg>
+  </div>
+  </div></div>
           ))}
         </div>
       </div>
